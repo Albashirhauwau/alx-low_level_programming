ac2c85873d81a37acc80e5fc1b91b1f4e462edf0
Albashirhauwau <hauwaualbashir@gmail.com>
2023-05-21 22:57:10 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  alphabet in lowercase
+ *
+ * Return: Always return 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int ch ='a';
+
+		while (ch <= 'z');
+		{
+			putchar(ch);
+			ch++;
+		}
+			putchar('\n');
+			return (0);
+}
+		
